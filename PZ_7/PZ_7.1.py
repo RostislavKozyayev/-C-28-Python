1b822c2f1ebd135f10f6141da2912d673e5541bb
RostislavKozyayev <rossttiislav@gmail.com>
2024-11-29 10:25:00 +0300

    PZ_7 and Report 7 is ready

---
+++ PZ_7/PZ_7.1.py
@@ -0,0 +1,10 @@
+# Дана строка. Подсчитать количество содержащихся в ней прописных латинских букв.
+
+# Создание функции, которая будет считывать и суммировать буквы, если буква в строке прописная и латинская:
+def count_uppercase_letters(s):
+    count = sum(1 for char in s if char.isupper() and 'A' <= char <= 'Z')
+    return count
+
+string = "Hello, World!"
+uppercase_count = count_uppercase_letters(string)
+print(f"Количество прописных латинских букв: {uppercase_count}")
---
+++ PZ_7/PZ_7.2.py
@@ -0,0 +1,11 @@
+# Дана строка-предложение на русском языке. Подсчитать количество содержащихся в строке гласных букв.
+
+# Создание функции, которая будет считывать и суммировать гласные буквы из строки:
+def count_vowels_russian(s):
+    vowels = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ'
+    count = sum(1 for char in s if char in vowels)
+    return count
+
+russian_sentence = "Привет, Мир!"
+vowels_count = count_vowels_russian(russian_sentence)
+print(f"Количество гласных букв: {vowels_count}")
